3fe76311eaed2072532a7b30e5fcc9d20e37ebb5
Ladzi031 <ladziladzani@gmail.com>
2024-07-10 20:06:55 +0200

    auto-wire byName machinery practice

---
+++ spring-baby/src/main/java/com/learn/autobyname/Address.java
@@ -0,0 +1,22 @@
+package com.learn.autobyname;
+
+public class Address {
+	private String street;
+	
+	
+	
+	public Address(String street) {
+		super();
+		this.street = street;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	
+}
